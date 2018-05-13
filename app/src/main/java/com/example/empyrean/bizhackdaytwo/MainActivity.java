2feaa9e1ca3dd13637f99d1d8ef39a9a04c64333
empyrean <tanya.taran1997@mail.ru>
2018-05-13 11:53:57 +0300

    settings firebase

--- app/src/main/java/com/example/empyrean/bizhackdaytwo/MainActivity.java
+++ app/src/main/java/com/example/empyrean/bizhackdaytwo/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.empyrean.bizhackdaytwo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
 }
