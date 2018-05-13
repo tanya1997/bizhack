b7f4f08df4b7b35ef28a9758ae4843ff1b5941db
Alexander <alexanpro@gmail.com>
2018-05-13 12:37:38 +0300

    fix

--- app/src/main/java/com/example/empyrean/bizhackdaytwo/MainActivity.java
+++ app/src/main/java/com/example/empyrean/bizhackdaytwo/MainActivity.java
@@ -3,29 +3,23 @@ package com.example.empyrean.bizhackdaytwo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-<<<<<<< HEAD
 import android.view.View;
 import android.widget.Button;
-
-public class MainActivity extends AppCompatActivity {
-    Button btnShop;
-    Button btnTrade;
-    Button btnCash;
-=======
 import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
-public class MainActivity extends AppCompatActivity{
->>>>>>> dev
+public class MainActivity extends AppCompatActivity {
+    Button btnShop;
+    Button btnTrade;
+    Button btnCash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-<<<<<<< HEAD
         btnShop = findViewById(R.id.btnShop);
         btnCash = findViewById(R.id.btnCash);
         btnTrade = findViewById(R.id.btnTrade);
@@ -53,9 +47,5 @@ public class MainActivity extends AppCompatActivity{
                 startActivity(intent);
             }
         });
-=======
->>>>>>> dev
     }
-
-
 }
